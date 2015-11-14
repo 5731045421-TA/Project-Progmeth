7e2dd88aab8ba93b799659472eb3590990e0b4bd
Teerapord Lin <ropcj@hotmail.com>
2015-11-14 17:39:05 +0700

    tower IRenderable

--- src/Character/Tower.java
+++ src/Character/Tower.java
@@ -1,14 +1,23 @@
 package Character;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import Game.Main;
 import Game.Player;
+import Graphic.IRenderable;
+import Graphic.Resource;
 
-public class Tower implements Buyable,Shootable,Upgrateable{
+public class Tower implements Buyable,Shootable,Upgrateable,IRenderable{
 	protected int attack;
 	protected int radius;
 	protected int price;
 	protected int[] upgradePrice;
 	protected int state;
+	public BufferedImage towerImage;
 	public Tower() {
 		this.attack = 5;
 		this.radius = 2;
@@ -45,6 +54,25 @@ public class Tower implements Buyable,Shootable,Upgrateable{
 	public boolean canBuy() {
 		if(Player.money >= price) return true;
 		return false;
+	}
+
+	@Override
+	public void draw(Graphics2D g2d) {
+		// TODO Auto-generated method stub
+		try{
+			ClassLoader load = Resource.class.getClassLoader();
+			towerImage = ImageIO.read(load.getResource("fileName"));
+		}
+		catch(IOException e){
+			towerImage = null;
+			
+		}
+	}
+
+	@Override
+	public int getZ() {
+		// TODO Auto-generated method stub
+		return Integer.MAX_VALUE;
 	}
 
 
---
+++ src/Graphic/IRenderable.java
@@ -0,0 +1,8 @@
+package Graphic;
+
+import java.awt.Graphics2D;
+
+public interface IRenderable {
+ public void draw(Graphics2D g2d);
+ public int getZ();
+}
